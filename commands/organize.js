4d1e0da871b1cb7d82586b4bb48859109bcd8a96
Ratnadeep Ghosh <ratnadeepghosh36@gmail.comm>
2022-03-19 20:16:39 +0530

    orgaize blueprint implemented

--- commands/organize.js
+++ commands/organize.js
@@ -32,7 +32,44 @@ function organize(srcPath) {
   let allFiles = fs.readdirSync(srcPath);
   // console.log(allFiles);
 
-
-
+//4.trvaerse over all the files and classify them on the basis of their extension (.pdf , .mp3)
+for (let i = 0; i < allFiles.length; i++){
+    // let ext = allFiles[i].;
+    //extname returns the extension of the file
+    let fullPathOfFile = path.join(srcPath, allFiles[i]);
+    console.log(fullPathOfFile);
+    //1. check if it is a file or folder
+    //lstatsync gives the information regarding the link provided ,
+    let isThisAFile = fs.lstatSync(fullPathOfFile).isFile(); //true-> file hai to  or false-> agar folder h 
+    console.log(allFiles[i] + " is " + isThisAFile);
+    if (isThisAFile) {
+      //1.1 get ext name
+      let ext = path.extname(allFiles[i]).split(".")[1];
+      // console.log(ext);
+      //1.2 get folder name from extension
+      let folderName = getFolderName(ext); //archives
+      // console.log(folderName);
+      //1.3 copy from src folder (srcPath) and paste in dest folder (folder_name e.g. document, media etc)
+                      //copy      kya copy kro    paste
+      copyFileToDest(srcPath, fullPathOfFile, folderName);
+    }
+  }
 
 }
+
+
+function getFolderName(ext) {
+    //magic
+    
+    return folderName;
+  }
+  
+  function copyFileToDest(srcPath, fullPathOfFile, folderName) {
+   
+    //magic
+  }
+  
+
+
+let srcPath="F:\FJP-5\fileOrganizer\downloads"
+organize(srcPath);
